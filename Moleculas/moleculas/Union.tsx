46b7733055b81d86320efec4cd0fa81963a4a9d9
mishell diaz <mishelldiaz400@gmail.com>
2025-05-03 09:35:03 -0400

    Inicio del examen

---
+++ Moleculas/moleculas/Union.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Titulo1 from "../Atomos/Titulo1";
+import Titulo2 from "../Atomos/Titulo2";
+
+const Union = () => {
+  return (
+    <>
+      <Titulo1 />
+      <Titulo2 />
+    </>
+  );
+};
+export default Union;
---
+++ Moleculas/organismos/Global.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Play from "../Atomos/Play";
+import Union from "../moleculas/Union";
+import Img from "../Atomos/Img";
+
+const Global = () => {
+  return (
+    <>
+    <div className="contenedor">
+      <Img />
+      <Union />
+      <Play />
+    </div>
+    </>
+  );
+};
+
+export default Global;
